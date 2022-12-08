d0374506ce5b88f884b49567543e15e83ae37985
Talles Dias <tallesdelfino1@gmail.com>
2022-12-08 15:22:45 -0300

    create class AplicationUser

--- Entities/Entities/ApplicationUser.cs
+++ Entities/Entities/ApplicationUser.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Entities.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,9 @@ namespace Entities.Entities
 {
     public class ApplicationUser : IdentityUser
     {
+        [Column("USR_CPF")]
+        public string CPF { get; set; }
+        [Column("USR_TIPO")]
+        public TypeUser? Tipo { get; set; }
     }
 }
---
+++ Entities/Entities/Notifies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class Notifies
+    {
+        [NotMapped]
+        public string NameProperty { get; set; }
+        [NotMapped]
+        public string Message { get; set; }
+        [NotMapped]
+        public List<Notifies> Notifications { get; set; }   
+    }
+}
