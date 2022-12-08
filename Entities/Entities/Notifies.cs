232b38905ec3f2489f8b61b1775c6af26b71d170
Talles Dias <tallesdelfino1@gmail.com>
2022-12-08 15:40:57 -0300

    create notifies

--- Entities/Entities/Notifies.cs
+++ Entities/Entities/Notifies.cs
@@ -9,11 +9,45 @@ namespace Entities.Entities
 {
     public class Notifies
     {
+        public Notifies()
+        {
+            Notifications = new List<Notifies>();
+        }
+
         [NotMapped]
         public string NameProperty { get; set; }
         [NotMapped]
         public string Message { get; set; }
         [NotMapped]
-        public List<Notifies> Notifications { get; set; }   
-    }
+        public List<Notifies> Notifications { get; set; }
+
+
+      public bool ValidateStringProperty(string value, string nameProperty)
+        {
+            if (string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(nameProperty))
+            {
+                Notifications.Add(new Notifies
+                {
+                    Message = "Campo Obrigatório",
+                    NameProperty = nameProperty
+                });
+                return false;
+            }
+            return true;
+        }
+
+        public bool ValidateIntProperty(int value, string nameProperty)
+        {
+            if (value < 1 || string.IsNullOrWhiteSpace(nameProperty))
+            {
+                Notifications.Add(new Notifies
+                {
+                    Message = "Campo Obrigatório",
+                    NameProperty = nameProperty
+                });
+                return false;
+            }
+            return true;
+        }
+    }  
 }
