e805e52e61cbe9bb85c877796c9f205cfd6762e7
Talles Dias <tallesdelfino1@gmail.com>
2022-12-09 19:05:00 -0300

    crate infra

--- Infrastructure/Configuration/ContextBase.cs
+++ Infrastructure/Configuration/ContextBase.cs
@@ -1,5 +1,6 @@
 ﻿using Entities.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,29 @@ namespace Infrastructure.Configuration
 {
     public class ContextBase : IdentityDbContext<ApplicationUser>
     {
+        public ContextBase(DbContextOptions<ContextBase> options):base(options)
+        {}
+        
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+        public string ObterStringConexao()
+        {
+            return "Data Source=localhost\\SQLEXPRESS;Initial Catalog=API_DDD_2022;Integrated Security=False;User ID=sa;Password=1234;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+        }
     }
 }
