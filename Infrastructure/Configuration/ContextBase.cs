a6c9a41887f638c30e5774022a9898d46b9b3b6f
Talles Dias <tallesdelfino1@gmail.com>
2022-12-09 11:06:28 -0300

    creating domain classes

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+
+    }
+}
