4afde2aa39a30f0e21ee4ce06b5f147213cb4f7a
Talles Dias <tallesdelfino1@gmail.com>
2022-12-12 16:29:36 -0300

    program

--- WebAPIs/Program.cs
+++ WebAPIs/Program.cs
@@ -1,6 +1,17 @@
+using Domain.Interfaces.Generics;
+using Domain.Interfaces;
 using Entities.Entities;
 using Infrastructure.Configuration;
+using Infrastructure.Repository.Generics;
+using Infrastructure.Repository.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Domain.Interfaces.InterfaceServices;
+using Domain.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using WebAPIs.Token;
+using AutoMapper;
+using WebAPIs.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,13 +28,57 @@ builder.Services.AddDbContext<ContextBase>(options =>
                   builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
-//config identity
+//config identity --> usuario que vamos utilizar
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ContextBase>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 //config services
+builder.Services.AddSingleton(typeof(IGeneric<>), typeof(RepositoryGenerics<>));
+builder.Services.AddSingleton<IMessage, RepositoryMessage>();
+
+// SERVIÇO DOMINIO
+builder.Services.AddSingleton<IServiceMessage, ServiceMessage>();
+
+// JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+      .AddJwtBearer(option =>
+      {
+          option.TokenValidationParameters = new TokenValidationParameters
+          {
+              ValidateIssuer = false,
+              ValidateAudience = false,
+              ValidateLifetime = true,
+              ValidateIssuerSigningKey = true,
+
+              ValidIssuer = "Teste.Securiry.Bearer",
+              ValidAudience = "Teste.Securiry.Bearer",
+              IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678")
+          };
+
+          option.Events = new JwtBearerEvents
+          {
+              OnAuthenticationFailed = context =>
+              {
+                  Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                  return Task.CompletedTask;
+              },
+              OnTokenValidated = context =>
+              {
+                  Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                  return Task.CompletedTask;
+              }
+          };
+      });
+var config = new AutoMapper.MapperConfiguration(cfg =>
+{
+    cfg.CreateMap<MessageViewModel, Message>();
+    cfg.CreateMap<Message, MessageViewModel>();
+});
+
+IMapper mapper = config.CreateMapper();
+builder.Services.AddSingleton(mapper);
 
 var app = builder.Build();
 
@@ -33,11 +88,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+// para dizer quais locais podem acessar 
+//var urlDev = "https://dominiodocliente.com.br";
+//var urlHML = "https://dominiodocliente2.com.br";
+//var urlPROD = "https://dominiodocliente3.com.br";
+//app.UseCors(b => b.WithOrigins(urlDev, urlHML, urlPROD));
+
+var devClient = "http://localhost:4200";
+app.UseCors(x => x
+.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader().WithOrigins(devClient));
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseSwaggerUI();
 
 app.Run();
